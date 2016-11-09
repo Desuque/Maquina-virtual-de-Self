1103000fc34915607276243661ae7035b921afe4
NicolasLercari <niko06_14@hotmail.com>
2016-11-09 15:59:14 -0300

    agrego los .cpp que eran .cc antes

---
+++ interfaz/morph.cpp
@@ -0,0 +1,282 @@
+#include "morph.h"
+#include "slot.h"
+#include "referencia.h"
+#include <gtkmm.h>
+#include <pangomm/layout.h>
+
+Morph::Morph(double posX, double posY) : m_TextView(nullptr), refTextViewConsola(nullptr) {
+	this->posX = posX;
+	this->posY = posY;
+	this -> nombreObjeto = "que?";
+	this->referencia=nullptr;
+}
+
+Morph::Morph(double posX, double posY, int width, int height,Gtk::TextView* m_TextView){
+	this->posX = posX;
+	this->posY = posY;
+	this->width = width;
+	this->height = height;
+	this -> nombreObjeto = "";
+	this->m_TextView=m_TextView;
+	refTextViewConsola = Gtk::TextBuffer::create();
+	refTextViewConsola->set_text("");
+	this->referencia=nullptr;
+}
+
+Morph::Morph(std::string nombreObjeto, double posX, double posY) : 
+	nombreObjeto(nombreObjeto), posX(posX), posY(posY), referencia(nullptr), m_TextView(nullptr) {
+	Pango::FontDescription font;
+	font.set_family("Monospace");
+	font.set_weight(Pango::WEIGHT_BOLD);
+	// http://developer.gnome.org/pangomm/unstable/classPango_1_1Layout.html
+	auto layout = create_pango_layout(this->nombreObjeto);
+	layout->set_font_description(font);
+	int text_width;
+	int text_height;
+	layout->get_pixel_size(text_width, text_height);
+	this->width = 2*text_width;
+	this->height = 2*text_height;
+	refTextViewConsola = Gtk::TextBuffer::create();
+	refTextViewConsola->set_text("");
+
+}
+
+// SE USA ESTE SIEMPRE VER LOS DEMAS ELIMINAR
+Morph::Morph(std::string nombreObjeto, double posX, double posY, Gtk::TextView* m_TextView, Gtk::TextView* codigoAsociado) : 
+		nombreObjeto(nombreObjeto), posX(posX), 
+		posY(posY), m_TextView(m_TextView),
+		textViewCodigoAsociado(codigoAsociado),
+		referencia(nullptr) {
+	Pango::FontDescription font;
+	font.set_family("Monospace");
+	font.set_weight(Pango::WEIGHT_BOLD);
+	// http://developer.gnome.org/pangomm/unstable/classPango_1_1Layout.html
+	auto layout = create_pango_layout(this->nombreObjeto);
+	layout->set_font_description(font);
+	int text_width;
+	int text_height;
+	layout->get_pixel_size(text_width, text_height);
+	this->width = 2*text_width;
+	this->height = 2*text_height;
+	refTextViewConsola = Gtk::TextBuffer::create();
+	refTextViewConsola->set_text("");
+	refTextViewCodigoAsociado = Gtk::TextBuffer::create();
+	refTextViewCodigoAsociado->set_text("");
+	//referencia = new Morph(posX-8,posY,8,8,m_TextView);
+}
+
+Morph::Morph(std::string nombreObjeto, double posX, double posY, int width, int height) 
+	: m_TextView(nullptr) {
+	Pango::FontDescription font;
+	font.set_family("Monospace");
+	font.set_weight(Pango::WEIGHT_LIGHT);
+	// http://developer.gnome.org/pangomm/unstable/classPango_1_1Layout.html
+	auto layout = create_pango_layout(this->nombreObjeto);
+	layout->set_font_description(font);
+	int text_width;
+	int text_height;
+	layout->get_pixel_size(text_width, text_height);
+	this->height = text_height;
+	this->posX = posX;
+	this->posY = posY;
+	this->width = width;
+	this->nombreObjeto = nombreObjeto;
+	refTextViewConsola = Gtk::TextBuffer::create();
+	refTextViewConsola->set_text("");
+	this->referencia=nullptr;
+	//referencia = new Morph(posX-8,posY,8,8,m_TextView);
+}
+
+Morph::Morph(){
+}
+
+Morph* Morph::get_it(){
+	std::string objeto;
+	std::string mensaje;
+	if (!m_TextView) std::cout << "error" << std::endl;
+	refTextViewConsola = m_TextView -> get_buffer();
+	std::string str = m_TextView->get_buffer()->get_text();
+	std::istringstream iss(str);
+	iss >> std::noskipws;
+	iss >> objeto  >> mensaje;  
+	std::cout << objeto << std::endl;
+	//Morph* morph = nullptr;
+	if (str != ""){
+		//morph = new Morph("que?",200,200,m_TextView);
+		this->nombreObjeto=objeto;
+		
+	}/* else {
+		morph = new Morph(objeto,200,200,m_TextView);
+	}*/
+
+	// ENVIAR A SERVIDOR
+	std::cout << this->nombreObjeto << " " << str
+			  << std::endl;
+
+	return this;
+}
+
+void Morph::do_it(){
+	std::string objeto;
+	std::string mensaje;
+	if (!m_TextView) std::cout << "error" << std::endl;
+	refTextViewConsola = m_TextView -> get_buffer();
+	std::string str = m_TextView->get_buffer()->get_text();
+	std::istringstream iss(str);
+	iss >> std::noskipws;
+	iss >> objeto  >> mensaje;  
+	std::cout << objeto << std::endl;
+	// ENVIAR A SERVIDOR
+	std::cout << this->nombreObjeto << " " << str
+			  << std::endl;
+}
+
+
+
+void Morph::agregarSlot(std::string nombreSlot){
+	//Morph* morph = new Morph(nombreSlot, this->posX, (this->posY)+(this->height), this->width, this->height);
+	Slot* slot = new Slot(nombreSlot, this->posX, (this->posY)+(this->height), this->width, this->height);
+	//this->height+=morph->height;
+	this->height+=slot->height;
+	slots.push_back(slot);
+	//slots.push_back(morph);
+}
+
+void Morph::actualizarAlturaMorph(size_t alturaDeSlot){
+	this->height += alturaDeSlot;
+}
+
+
+void Morph::agregarSlot(InterfaceSlot* interface_slot){
+	Slot* slot = new Slot(interface_slot,this->posX, (this->posY)+(this->height), this->width, this->height);
+	this->actualizarAlturaMorph(slot->get_height());
+	slots.push_back(slot);
+}
+
+void Morph::draw(const Cairo::RefPtr<Cairo::Context>& cr){
+
+	cr->set_line_width(2.5);
+	cr->set_source_rgb(0.0, 0.0, 0.0);
+	
+	cr -> rectangle(this->posX, this -> posY,this->width, this->height);
+	//cr ->fill();
+	cr-> stroke();
+
+	for (int i=0; i < slots.size(); ++i){
+		//slots[i] -> draw_slot(cr);
+		slots[i] -> draw(cr);
+		//morph->width += text_width;
+		//this->height += slots[i]->height;
+	}
+
+	Pango::FontDescription font;
+	font.set_family("Monospace");
+	font.set_weight(Pango::WEIGHT_BOLD);
+	auto layout = create_pango_layout(this->nombreObjeto);
+
+	int text_width;
+	int text_height;
+
+	layout->get_pixel_size(text_width, text_height);
+	
+	cr->move_to((this->posX)+(text_width/2), (this->posY)+(text_height/2));
+
+	layout->show_in_cairo_context(cr);
+}
+
+Morph* Morph::clikEnObtenerSlot(int posX,int posY){     
+	for (int i=0; i < slots.size(); ++i){         // guardar cuando agregue poli         
+		if ((posX >= (slots[i]->posX) + (slots[i]->width) -10) && (posY >= (slots[i]->posY) + 2)
+			&& (posX <= (((slots[i]->posX) + (slots[i]->width))-2)) && (posY <=
+			((slots[i]->posY)+slots[i]->height-2))) {   
+				Morph* morph = nullptr;
+				if(slots[i]->code){
+					morph = new Morph(slots[i]->name,slots[i]->posX + slots[i]->width +20, slots[i]->posY+4,m_TextView,textViewCodigoAsociado);             
+					morph -> refTextViewCodigoAsociado->set_text(slots[i]->value);
+				} else {
+					morph = new Morph(slots[i]->value,slots[i]->posX + slots[i]->width +20, slots[i]->posY+4,m_TextView,textViewCodigoAsociado);             
+				}
+				//Morph* morph = new (slots[i]->value,slots[i]->posX + slots[i]->width +20, slots[i]->posY+4,m_TextView);             
+				//morph -> referencia = new Morph(morph->posX-8,morph->posY,8,8,m_TextView);                   
+				morph -> referencia = new Referencia(morph, slots[i]);
+				//slots[i] -> referencia = morph->referencia;
+				//morph -> referencia -> slotPadre = morph;
+				//slots[i] -> referencia -> slotPadre = morph;
+				return morph;         
+			}     
+		}     
+	return nullptr; 
+}
+
+Slot* Morph::obtenerSlot(int posX,int posY){     
+	for (int i=0; i < slots.size(); ++i){         
+		// guardar cuando agregue poli         
+		if ((posX >= (slots[i]->posX) + (slots[i]->width) -10) && (posY >= (slots[i]->posY) + 2)
+			&& (posX <= (((slots[i]->posX) + (slots[i]->width))-2)) && (posY <=
+			((slots[i]->posY)+slots[i]->height-2))) {   
+				return slots[i];     
+			}     
+		}     
+	return nullptr; 
+}
+
+Morph* Morph::clickEnReferenciaAMorph(int posX,int posY){
+		/*if ((posX >= this->posX) && (posY >= this->posY)
+	    		&& (posX <= ((this->posX)+8)) && (posY <= ((this->posY)+8)))
+		{
+			std::cout << " referencia a morph" << std::endl;		
+      		//Morph* morph = new Morph("",this->posX-8,this->posY,m_TextView);
+      		referencia=nullptr;
+      		return referencia;
+      	}*/
+	return nullptr;
+}
+
+void Morph::mostrarDescripcionMorph(){
+	//Glib::RefPtr<Gtk::TextBuffer> m_refTextBuffer1;
+	//m_refTextBuffer1 = Gtk::TextBuffer::create();
+	//refTextViewConsola->set_text("");
+	if (m_TextView && refTextViewConsola){
+		m_TextView -> set_buffer(refTextViewConsola);
+	}
+	if (textViewCodigoAsociado && refTextViewCodigoAsociado){
+		textViewCodigoAsociado->set_buffer(refTextViewCodigoAsociado);
+	}
+}
+
+void Morph::actualizar_posicion(double x, double y){
+	
+	// para no dibujar fuera de la ventana
+	/*if (x + width > 400 || y + height > 800){
+		if (x + width > 400 ){
+			this->posX = 400;
+		} 
+		if (y + height > 800 ){
+			this -> posY = 800;
+		}
+		return;
+	}
+	if (x < 0 || y < 0){
+		if (x < 0){
+			this->posX = 0;
+		}
+		if (y < 0){
+			this -> posY = 0;
+		}
+		return;
+	}*/
+	for (int i=0; i < slots.size(); ++i){
+		slots[i] -> posX = x;
+		slots[i] -> posY = y + (slots[i]->posY - this->posY);
+	}
+	if(referencia != nullptr){
+		referencia->posX=x-8;
+		referencia->posY=y;
+	}
+	this->posX = x;
+	this->posY = y;		
+}
+
+
+
+Morph::~Morph(){}
---
+++ interfaz/myarea.cpp
@@ -0,0 +1,337 @@
+#include "myarea.h"
+#include <iostream>
+#include <cairomm/context.h>
+#include <cairomm/enums.h>
+#include "morph.h"
+#include "slot.h"
+#include "../vm/client_interface_slot.h"
+#include "../vm/client_json_reader.h"
+
+#define CREAR_MAQUINA_VIRTUAL 0x01
+
+MyArea::MyArea()
+{
+}
+
+MyArea::MyArea(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& builder)
+: Gtk::DrawingArea(cobject),
+  m_builder(builder),
+  m_pButton(nullptr),
+  m_pButton2(nullptr),
+  m_TextView(nullptr),
+  textViewCodAsociado(nullptr)
+{
+  add_events(Gdk::BUTTON_PRESS_MASK |
+         Gdk::BUTTON_RELEASE_MASK |
+         Gdk::SCROLL_MASK |
+         Gdk::SMOOTH_SCROLL_MASK |
+         Gdk::POINTER_MOTION_MASK);
+  // ::: Create popup menu :::
+
+  // conecto agregarSlot
+  MenuItemAgregarSlot.set_label("Agregar Slot");
+  MenuItemAgregarSlot.signal_activate().connect(sigc::mem_fun(*this,&MyArea::agregarSlot_event));
+  m_Menu_Popup.append(MenuItemAgregarSlot);
+
+  m_Menu_Popup.show_all();
+
+  // Connect the menu to this Widget
+  //m_Menu_Popup.accelerate(*this);
+  
+  m_builder-> Gtk::Builder::get_widget("LabelnombreObjeto", lNombreObjeto);
+  if (lNombreObjeto == NULL) std::cout << "error" << std::endl;
+
+  m_builder-> Gtk::Builder::get_widget("button1", m_pButton);
+  if (m_pButton == NULL) std::cout << "error" << std::endl;
+  m_pButton->signal_clicked().connect( sigc::mem_fun(*this,&MyArea::get_it_event));
+
+  m_builder-> Gtk::Builder::get_widget("button2", m_pButton2);
+  if (m_pButton2 == NULL) std::cout << "error" << std::endl;
+  m_pButton2->signal_clicked().connect( sigc::mem_fun(*this,&MyArea::do_it_event));
+
+  m_builder-> Gtk::Builder::get_widget("button3", m_pButton3);
+  if (m_pButton3 == NULL) std::cout << "error" << std::endl;
+  m_pButton3->signal_clicked().connect( sigc::mem_fun(*this,&MyArea::close_event));
+
+  m_builder-> Gtk::Builder::get_widget("textview2", m_TextView);
+  if (m_TextView == NULL) std::cout << "error" << std::endl;
+
+  m_builder-> Gtk::Builder::get_widget("textview1", textViewCodAsociado);
+  if (textViewCodAsociado == nullptr) std::cout << "error" << std::endl; 
+	
+  resetFlag=true;
+  queue_draw();
+  moveFlag=false;
+
+  try{
+    proxyServer.connect("127.0.0.1",8080);
+    proxyServer.enviar(CREAR_MAQUINA_VIRTUAL,sizeof(char));
+    //uint32_t lobbyId = proxyServer.recibirId(sizeof(uint32_t));
+    // cambiar recibirId por recibirTam
+    //uint32_t tamMensaje = proxyServer.recibirId(sizeof(uint32_t));
+    std::string infoSlots = proxyServer.recibirSlotsDe("lobby");
+
+    /***DECODIFICACION DEL JSON (CLIENTE)***/
+    std::vector<InterfaceSlot*> i_slots;
+    JsonReader slots_reader;
+    slots_reader.read(i_slots, infoSlots);
+
+    Morph* lobby= new Morph("lobby",10.,10.,m_TextView, textViewCodAsociado);
+    actual=lobby;
+    lNombreObjeto -> set_text(actual->nombreObjeto);
+    morphs.push_back(lobby);
+
+    int size = i_slots.size();
+    for (int i = 0; i < size ; i++){
+      i_slots[i] -> print_attr();
+      actual->agregarSlot(i_slots[i]);
+    }
+    
+    for (std::vector<InterfaceSlot*>::iterator it = i_slots.begin(); it != i_slots.end();){  
+      delete* it;  
+      it = i_slots.erase(it);
+    }
+
+  } catch (const std::exception &e) {
+    Morph* lobby = new Morph("lobby",10.,10.,m_TextView,textViewCodAsociado);
+    actual=lobby;
+    lNombreObjeto -> set_text(actual->nombreObjeto);
+    morphs.push_back(lobby);    
+  }
+
+}
+
+MyArea::MyArea(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& builder, bool warning)
+: MyArea(cobject,builder){
+
+}
+
+void MyArea::liberarMemoria(){
+  std::cout << "libero memoria de morphs" << std::endl;
+  for (int i=0; i < morphs.size(); ++i){  
+    delete morphs[i];
+  }
+}
+
+
+MyArea::~MyArea()
+{
+}
+
+void MyArea::botonGuardarNuevoSlotEvent(){
+  std::cout << "guardar slot" << std::endl;
+
+  Gtk::TextView* textSlot = nullptr;
+  m_builder-> Gtk::Builder::get_widget("textview3", textSlot);
+  if (textSlot == nullptr) std::cout << "error" << std::endl;
+  std::string nombreSlot = textSlot->get_buffer()->get_text();
+  actual -> agregarSlot(nombreSlot);
+  
+  // enviar esto al server.
+  std::cout << actual->nombreObjeto << " __addSlots:(|"
+            << nombreSlot << "|)."  << std::endl;
+
+  sigcButtonGuardar.disconnect();
+  queue_draw();
+}
+
+void MyArea::agregarSlot_event(){
+  std::cout << "agregar slot" << std::endl;
+  Gtk::Button* botonGuardar = nullptr;
+  m_builder-> Gtk::Builder::get_widget("button4", botonGuardar);
+  if (botonGuardar == NULL) std::cout << "error" << std::endl;
+  sigcButtonGuardar = botonGuardar->signal_clicked().connect( sigc::mem_fun(*this,&MyArea::botonGuardarNuevoSlotEvent));
+}
+
+void MyArea::get_it_event(){
+  Morph* morph = new Morph("que?",200,200,m_TextView,textViewCodAsociado);
+  actual = morph->get_it();
+  //morph->nombreObjeto=objeto;
+  morphs.push_back(actual); 
+  actual -> mostrarDescripcionMorph();
+  queue_draw();
+}
+
+void MyArea::do_it_event(){
+  actual -> do_it();
+}
+
+void MyArea::close_event(){
+  if (actual == nullptr) return;
+  for (int i =0; i < morphs.size() ; ++i){
+      if(*(morphs[i]) == *actual){
+        // preguntar si alguien lo apunta 
+        // para borrar flechas
+        delete morphs[i];
+        morphs.erase(morphs.begin()+i);
+        break;
+      }
+  }
+  actual = nullptr;
+  queue_draw();
+}
+
+//released : process mouse button event
+bool MyArea::on_button_release_event(GdkEventButton *event)
+{
+  // Check if it is the left button
+  if (event->button==1 && moveFlag)
+  {
+    // agrega la referencia para el drag and drop
+    // faltaria borrar
+    if (refenciaActual){
+      for (int i =0; i < morphs.size() ; ++i){
+        if(*(morphs[i]) == Morph(event->x,event->y)){
+          //refenciaActual-> apuntaAEsteMorph(morphs[i]);
+          morphs[i]->referencias.push_back(refenciaActual);
+          //refenciaActual ->apuntoAMorph -> referencia = nullptr;
+          //morphs[i]->agregarReferencia(refenciaActual);
+          //refenciaActual-> borrarReferenciaAnterior();
+        }
+      }
+    } 
+    /*for (int i =0; i < morphs.size() ; ++i){
+    //draw_text(cr, morphs[i].first, morphs[i].second);
+      if(*(morphs[i]) == Morph(event->x,event->y)){
+        // PREGUNTAR ESUNAREFERENCIA
+        if(*actual != *morphs[i] && actual->width == 8
+              && actual->height==8 && morphs[i]->referencia!=actual){
+          //morphs[i]->slots.push_back(actual);
+          std::cout << "ahora apunto a: "<< morphs[i]->nombreObjeto << std::endl;
+          std::cout << " actual -> slotPadre: " << actual->slotPadre->nombreObjeto<< std::endl;
+          
+          actual -> slotPadre -> referencia = nullptr;
+          morphs[i]->referencia = actual;
+          //actual -> slotPadre = morphs[i];
+          //actual = morphs[i];
+          break;
+        }
+      }
+    }*/
+    // End of motion
+    moveFlag=false;    
+    // Update display
+    queue_draw();
+    return true;
+  }
+  // Event has been handled
+  return false;
+}
+
+// Mouse button pressed : process mouse button event
+bool MyArea::on_button_press_event(GdkEventButton *event)
+{
+  // Check if the event is a left button click.
+  if (event->button == 1)
+  {
+    for (int i =0; i < morphs.size() ; ++i){
+     //draw_text(cr, morphs[i].first, morphs[i].second);
+      if(*(morphs[i]) == Morph(event->x,event->y)){
+        actual = morphs[i];
+        refenciaActual=nullptr;
+        Morph* morphDeSlot = actual->clikEnObtenerSlot(event->x,event->y);
+        if (morphDeSlot){
+          Slot* slot = actual-> obtenerSlot(event->x,event->y);
+          if (!slot){
+            std::cout << "error obtenerSlot devolvio null" << std::endl;
+          }
+          std::string infoSlots="";
+          
+          if (slot->value == "object"){
+            std::string infoSlots = proxyServer.recibirSlotsDe(slot->name);
+            std::cout << slot->name << std::endl;
+            std::cout << slot->value << std::endl;
+
+            //DECODIFICACION DEL JSON (CLIENTE)
+            std::vector<InterfaceSlot*> i_slots;
+            JsonReader slots_reader;
+            slots_reader.read(i_slots, infoSlots);
+
+            int size = i_slots.size();
+            for (int i = 0; i < size ; i++){
+              i_slots[i] -> print_attr();
+              morphDeSlot->agregarSlot(i_slots[i]);
+            }
+          }
+          morphs.push_back(morphDeSlot); 
+          if (morphDeSlot->referencia){ 
+           referencias.push_back(morphDeSlot->referencia);
+          }
+        }
+        offXMouse = actual->posX - event->x;
+        offYMouse = actual->posY - event->y;
+        // Start moving the view
+        moveFlag=true;
+        // va en el morph
+        lNombreObjeto->set_text(actual->nombreObjeto);
+        actual -> mostrarDescripcionMorph();
+        // Event has been handled
+        return true;      
+      }
+    }
+    for (int i =0; i < referencias.size() ; ++i){
+      if(*(referencias[i]) == Referencia(event->x,event->y)){
+          refenciaActual = referencias[i];
+          actual = nullptr;
+          offXMouse = refenciaActual->posX - event->x;
+          offYMouse = refenciaActual->posY - event->y;
+          // Start moving the view
+          moveFlag=true;
+      }
+    }
+    return false;
+  }
+  // Check if the event is a right button click.
+  if(event->button == 3)
+  {
+    for (int i =0; i < morphs.size() ; ++i){
+     //draw_text(cr, morphs[i].first, morphs[i].second);
+      
+      if(*(morphs[i]) == Morph(event->x,event->y)){
+        actual = morphs[i];
+        lNombreObjeto->set_text(actual->nombreObjeto);
+        actual -> mostrarDescripcionMorph();
+        m_Menu_Popup.popup(event->button, event->time);
+        // Event has been handled
+        return true;
+      }
+    }
+  }
+  // Event has not been handled
+  return false;
+}
+
+bool MyArea::on_motion_notify_event (GdkEventMotion*event)
+{
+
+  // If the left button is pressed, move the view
+  if (moveFlag){
+    // Get mouse coordinates
+    int XMouse=event->x;
+    int YMouse=event->y;
+    if(actual != nullptr){
+      actual->actualizar_posicion((event->x+offXMouse),(event->y+offYMouse ) );
+    }
+    if(refenciaActual != nullptr){
+      refenciaActual->actualizar_posicion((event->x+offXMouse),(event->y+offYMouse ) );
+    }
+    // Update view
+    queue_draw();
+    return true;
+  }
+  // Event has been handled
+  return false;
+}
+
+
+bool MyArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
+{  
+  for (int i =0; i < morphs.size() ; ++i){
+    morphs[i]->draw(cr);
+  }
+  for (int i =0; i < referencias.size() ; ++i){
+    referencias[i]->draw(cr);
+  }
+  return true;
+}
+
