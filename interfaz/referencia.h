5dd59ae0fadbdfb15449ebe9f19e2865f3819c3c
NicolasLercari <niko06_14@hotmail.com>
2016-11-26 18:59:41 -0300

    falto un archivo

--- interfaz/referencia.h
+++ interfaz/referencia.h
@@ -36,11 +36,10 @@ public:
 		cr-> stroke();
 	}
 
-	void actualizarValoresAMorphApuntado(Slot* unSlot){
+	void actualizarMorphApuntado(Slot* unSlot){
 		this->perteneceASlot = unSlot;
 		this->apuntoAMorph->actualizarValores(unSlot);
 	}
-
 
 	void setSlotEstaDibujadoComoMorph(bool flag){
 		this->perteneceASlot->setEstaDibujadoComoMorph(flag);
